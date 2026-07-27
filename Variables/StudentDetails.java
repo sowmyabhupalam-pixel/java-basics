9efe2bddc39d2a76941921d763cd1068eea2d851
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-27 19:52:26 +0530

    Create StudentDetails.java

---
+++ Variables/StudentDetails.java
@@ -0,0 +1,14 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        String name = "Sowmya";
+        int age = 20;
+        String branch = "CSE (AI)";
+        double cgpa = 8.11;
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Branch: " + branch);
+        System.out.println("CGPA: " + cgpa);
+    }
+}

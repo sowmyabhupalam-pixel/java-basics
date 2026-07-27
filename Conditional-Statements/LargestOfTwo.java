20d61760e738876106c99afa2283fd2c97edf5ea
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-27 20:00:20 +0530

    Create LargestOfTwo.java

---
+++ Conditional-Statements/LargestOfTwo.java
@@ -0,0 +1,12 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+
+        if (a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}

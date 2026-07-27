5b49043d3e4ca769d6f27c5cc12c6f636fb0c692
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-27 19:49:23 +0530

    Create DataTypes.java

---
+++ Variables/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}

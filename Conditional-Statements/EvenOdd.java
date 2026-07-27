96ad92673e28afc9788278c3dadb99db348a58bc
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-27 19:56:42 +0530

    Create EvenOdd.java

---
+++ Conditional-Statements/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int number = 8;
+
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}

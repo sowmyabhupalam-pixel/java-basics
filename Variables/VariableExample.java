b0b339228aea70360e3545bf2492656cb65cb652
Bhupalam Sai Sowmya <sowmyabhupalam@gmail.com>
2026-07-27 19:46:27 +0530

    Create VariableExample.java

---
+++ Variables/VariableExample.java
@@ -0,0 +1,16 @@
+public class VariableExample {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.2;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Sowmya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
